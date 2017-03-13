f9991e3f1efc24a0e28e26f7af205302b7c67bf8
Cristóbal Armaza <ca455@cornell.edu>
2017-03-12 20:18:25 -0400

    Update main.cpp

--- homeworks/3/main.cpp
+++ homeworks/3/main.cpp
@@ -90,10 +90,10 @@ int main()
         // From the initial conditions, compute the predictor for the interior points
         for(int i = 1; i < nx-1; i++)
         {
-            for(int j = 1; j < nx-1; j++)
+            for(int j = 1; j < ny-1; j++)
             {
                 rhos[ind(i, j)] = rho[ind(i, j)];// formulas in construction
-                rhous[ind(i, j)] = rhos[ind(i, j)];
+                rhous[ind(i, j)] = rhou[ind(i, j)];
                 rhovs[ind(i, j)] = rhov[in(i, j)];
             }
         }
